a798c9c52e7a75056aa85846e4ae508a16c192b6
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-03-15 15:31:17 +0100

    Setting first routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Route::get | post | delete | put
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('prueba', function () {
+    return "This a test route.";
+});
+
+Route::get('prueba/{dato}', function ($dato) {
+    return "Te regalo tu $dato";
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
