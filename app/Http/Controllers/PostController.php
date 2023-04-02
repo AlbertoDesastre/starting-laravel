d3030c3f2acd051d8eaeeb6010989e56c3670d49
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-04-02 10:35:11 +0200

    Añadir metodo para crear publicaciones en la base de datos, mediante asignacion masiva, configurandolo en modelo User que tiene muchos Post y en el modelo Post para decirle que asignaciones vamos a hacer.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+//Imports de mis cosillas
 use App\Models\Post;
+
+//Imports del Framework
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -15,6 +19,25 @@ class PostController extends Controller
 
     public function create(Post $post){
         return view("posts.create", ["post" => $post]);
+    }
+
+    // Como necesito recuperar lo que me envía un usuario lo recupero con Request
+    public function store(Request $request){
+        // Desarrollo el post a partir del user que se encuentra logeado. Esa info del user se encuentra dentro de la clase Request.
+        /* OJO! En el momento en el que escribo este comentario el método "posts" no existe en el modelo Usuario.
+        Esto significa que cuando intente acceder a ese método saltará un error. Es tan sencillo como incluir ahora el método
+        al que intento acceder un poco más abajo. */
+        $post = $request->user()->posts()->create([
+            "title" => $title = $request->title,
+            // Aqui vuelvo a utilizar el helper Str para hacer una URL amigable. Siempre que uso un
+            // helper de este tipo tengo que importarlo.
+            "slug" => Str::slug($title),
+            "body" => $request->body
+        ]);
+
+        /* Ojo al detalle! Primero salvo la información y LUEGO redirecciono. */
+
+        return redirect()->route("posts.edit", $post);
     }
 
     public function edit(Post $post) {
